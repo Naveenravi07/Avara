19117443b7f8847d174744e67244c852e0376954
Naveenravi07 <gamerkidnav@gmail.com>
2025-02-25 22:14:43 +0530

    Build fix

--- apps/web/src/hooks/useAuth.tsx
+++ apps/web/src/hooks/useAuth.tsx
@@ -29,7 +29,6 @@ const handleLogout = async () => {
     const data = await response.json();
     Cookie.remove('x-auth-cookie');
     console.log('logout success', data);
-        user = null;
   }
 };
 
