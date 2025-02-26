00626220fd5af044231577a76008804eb7325224
Naveenravi07 <gamerkidnav@gmail.com>
2025-02-26 13:04:15 +0530

    Session Test

--- apps/api/comon/decorators/current-user-decorator.ts
+++ apps/api/comon/decorators/current-user-decorator.ts
@@ -4,6 +4,8 @@ import { SessionUser } from '../../src/users/dto/session-user';
 export const CurrentUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): SessionUser => {
     const request = ctx.switchToHttp().getRequest();
+    console.log("\n Cookie for Req = ",request.cookies)
+    console.log("\n Req.user = ",request.user)
     return request.user ?? null;
   },
 );
